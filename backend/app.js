1ff8b7f8fcccf393f90981e5cb3811fef8bb8e5d
omanager2100 <150122677+omanager2100@users.noreply.github.com>
2025-07-11 20:39:14 +0200

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -129,4 +129,4 @@ app.post("/submit", async (req, res) => {
   }
 });
 
-app.listen(PORT, () => console.log(\"Server running on port \" + PORT));
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
